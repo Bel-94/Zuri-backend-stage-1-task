943ed76b313b2eea379379e06fa04042083cf577
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-10-31 10:31:26 +0300

    add the backend

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User,AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    slackUsername = models.CharField(max_length=30)
+    backend = models.BooleanField(default=False)
+    age = models.IntegerField()
+    bio = models.CharField(max_length=100)
