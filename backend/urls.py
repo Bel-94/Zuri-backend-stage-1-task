b6a7a56af0093237f89c7083a7ceeb781a5850aa
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-10-31 11:27:43 +0300

    create the user api endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # User API endpoints
+    # path('users/', views.UserList.as_view()),
+    # path('users/<int:pk>/', views.UserDetail.as_view()),
+
+    # User API endpoints
+    path('users/', views.UserList.as_view()),
+
 ]
