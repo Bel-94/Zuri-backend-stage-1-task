db2a6b7957ee568f17eb3f5660bb9b620a40b5b3
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-10-31 10:51:47 +0300

    run migrations

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User,AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
+class User(models.Model):
     slackUsername = models.CharField(max_length=30)
     backend = models.BooleanField(default=False)
     age = models.IntegerField()
