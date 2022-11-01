383676993f8483e582ca3fe8d92f3dd4976305c2
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-11-01 10:21:46 +0300

    rectify the url path

--- backend/urls.py
+++ backend/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # User API endpoints
-    # path('users/', views.UserList.as_view()),
-    # path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 
 ]
 
